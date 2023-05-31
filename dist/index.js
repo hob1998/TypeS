373066dd6b33bf6bdb0ebbc62d3cf28ae175cd69
hob1998 <hsobrien5019@gmail.com>
2023-05-31 15:02:17 -0700

    Inititilized reopository

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 20;
+if (age > 18)
+    age += 20;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age: number = 20;
+
+if (age > 18) 
+    age += 20;
+console.log(age);
